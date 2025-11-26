a994032187339a576cc1b19a9e9691e0cf6b72ad
_lordBucket <antoniuk.orest@gmail.com>
2025-11-26 18:37:51 +0100

    Temporary wrong url 3

--- shared/src/api/apiClient.ts
+++ shared/src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios, { type AxiosInstance } from 'axios';
 import { useAuthStore } from "../store/useAuthStore";
 
 const apiClient: AxiosInstance = axios.create({
-    baseURL: 'http://landapp.lordbucket.eu/api',
+    baseURL: 'https://landapp.lordbucket.eu/api',
     headers: {
         'Content-Type': 'application/json',
     }
